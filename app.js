0e7d4a8276382c31c3d294822fa6b4a38a886db8
mahmad-labib <mohamed.anwarlabib@gmail.com>
2021-09-25 16:03:19 +0200

    dd

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const koa = require("koa")
 const app = new koa();
 
-app.use(async  (ctx)=> {
+var _ = require("koa-route");
+var parse = require("co-body");
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
+app.use(_.post("/user", saveUser));
+// app.use(_.get("/user/:id", getUser));
+
+function* saveUser() {
+	var userFormRequest = yield parse(this);
+	var user = yield userFormRequest.insert(userromRequest);
+
+	this.body = user;
+	this.set("location", "/user/" + user._id);
+	this.status = 201
+}
+
+app.use(async (ctx) => {
 	ctx.body = "Hello World!";
 });
 
